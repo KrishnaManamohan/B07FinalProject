6afef82ff959b3ea86a9afa3dd8e739ba75e7bf3
Amaan1-1 <amaan.batla@mail.utoronto.ca>
2024-11-20 18:32:26 -0500

    Added firebase Authentication to MainActivity

--- app/src/main/java/com/example/planetzeapp/MainActivity.java
+++ app/src/main/java/com/example/planetzeapp/MainActivity.java
@@ -7,14 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
